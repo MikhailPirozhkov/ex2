468fecf32c7fc4faf3eebedf06ba0ec831f4a3da
Mikhail Pirozhkov <mipiyu@gmail.com>
2016-02-10 12:52:33 +0300

    create index file

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+//connect require classes
+require_once 'Human.php';
+require_once 'Boy.php';
+require_once 'Girl.php';
+
+//create 5 boy's group
+$boys = array(5);
+$boys = [	new Boy (Kyle),
+			new Boy (Stan),
+			new Boy (Kenny),
+			new Boy (Eric),
+			new Boy (Leopold)
+];
+
+//create 5 girl's group
+$girls = array(5);
+$girls = [	new Girl (),
+			
+];
